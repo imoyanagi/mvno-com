c61f18a712f9aeef76d1a0f378f265b517c257c6
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-03-08 12:48:09 +0900

    作成：トップ検索機能1

--- app/controllers/plans_controller.rb
+++ app/controllers/plans_controller.rb
@@ -1,12 +1,21 @@
 class PlansController < ApplicationController
 
 	def top
+		@q = Plan.ransack(params[:q])
+    	@plans = @q.result(distinct: true)
 	end
 
 
 	def index
+		@q = Plan.search(params[:q])
+    	@plans = @q.result(distinct: true)
 	end
 
 	def show
 	end
+
+	private
+	  def search_params
+	    params.require(:q).permit!
+	  end
 end
