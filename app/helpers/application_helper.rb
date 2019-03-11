44a4c9801e12a744af35222cf2adb7d1ccc909b9
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-03-11 14:52:26 +0900

    作成：換算機能

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,22 @@
 module ApplicationHelper
-	def total_initial_cost(plan, user_phone)
-		user_phone.carrier.penalty_bill + user_phone.carrier.mnp_bill + plan.carrier.initial_bill
-	end
+	def calc_mnp_costs(plan, options, user_phone)
+		@total_initial_cost = user_phone.carrier.penalty_bill + user_phone.carrier.mnp_bill + plan.carrier.initial_bill
 
-	def total_monthly_bill(plan, options, user_phone)
-		total_monthly_bill = 0
+		@total_monthly_bill = 0
 		options.each do |option|
-			total_monthly_bill += option.bill
+			@total_monthly_bill += option.bill
 		end
-		total_monthly_bill + plan.bill + user_phone.phone_bill
+		@total_monthly_bill += plan.bill + user_phone.phone_bill
+
+		@saved_cost_per_year = (user_phone.bill - @total_monthly_bill) * 12 - @total_initial_cost
 	end
 
-	def saved_cost_per_year(plan, options, user_phone)
-		total_monthly_bill = total_monthly_bill(plan, options, user_phone)
-		saved_cost_per_year = (user_phone.bill - total_monthly_bill) * 12 - total_initial_cost(plan, user_phone)
-	end
-
-	def convert_to_beer
-		beer_price= 200
+	def convert_to(saved_cost_per_year)
+		convert_to_items = [["缶ビール", 200, "缶"],["うまい棒",10, "本"],["牛丼並", 350, "杯"],["コンビニコーヒー", 100, "杯"],["映画館", 1800, "回"]]
+		select_item = convert_to_items[rand(5)]
+		@name = select_item[0]
+		price = select_item[1]
+		unit_name = select_item[2]
+		@number = "#{(saved_cost_per_year / price).to_i}" + "#{unit_name}"
 	end
 end
