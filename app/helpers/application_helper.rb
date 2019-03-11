74fe3403bc5dd1a7a301b3c89604a24a07ff8ffc
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-03-11 14:05:58 +0900

    作成：乗り換え計算ヘルパー

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,22 @@
 module ApplicationHelper
+	def total_initial_cost(plan, user_phone)
+		user_phone.carrier.penalty_bill + user_phone.carrier.mnp_bill + plan.carrier.initial_bill
+	end
+
+	def total_monthly_bill(plan, options, user_phone)
+		total_monthly_bill = 0
+		options.each do |option|
+			total_monthly_bill += option.bill
+		end
+		total_monthly_bill + plan.bill + user_phone.phone_bill
+	end
+
+	def saved_cost_per_year(plan, options, user_phone)
+		total_monthly_bill = total_monthly_bill(plan, options, user_phone)
+		saved_cost_per_year = (user_phone.bill - total_monthly_bill) * 12 - total_initial_cost(plan, user_phone)
+	end
+
+	def convert_to_beer
+		beer_price= 200
+	end
 end
