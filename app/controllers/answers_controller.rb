eeaab224401b3a0adf362753d29b758bdeb1d98b
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-03-16 17:50:29 +0900

    作成：回答投稿機能

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,9 +1,17 @@
 class AnswersController < ApplicationController
 
 	def create
+		answer = Answer.new(answer_params)
+		answer.user_id = current_user.id
+		answer.save!
+		redirect_to question_path(answer.question_id)
 	end
 
 	def destroy
 	end
 
+	private
+	def answer_params
+		params.require(:answer).permit(:body, :answer_id, :question_id)
+	end
 end
