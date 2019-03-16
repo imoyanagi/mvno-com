fda4464133e559005fb12f6599631bc9f2b49247
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-03-16 19:47:45 +0900

    追加：回答ナンバー割り振り機能

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -3,6 +3,7 @@ class AnswersController < ApplicationController
 	def create
 		answer = Answer.new(answer_params)
 		answer.user_id = current_user.id
+		answer.number = Answer.where(question_id: answer.question_id).count + 1
 		answer.save!
 		redirect_to question_path(answer.question_id)
 	end
