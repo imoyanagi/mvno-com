1d83aa649ff56f018de2b732a1d16506c089f1d0
井上達朗 <t.inoue@inouetatsurounoMacBook-puro.local>
2019-03-31 17:16:33 +0900

    追加：レビューエラーメッセージ

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,15 +8,22 @@ class ReviewsController < ApplicationController
 
   def create
   	review = Review.new(review_params)
-  	review.save
-  	redirect_to users_path
+    review.user_id = current_user.id
+  	if review.save
+  	   redirect_to plan_reviews_path(plan_id: review.plan_id)
+    else
+       redirect_to plan_path(id: review.plan_id), flash: { error: review.errors.full_messages }
+    end
   end
 
 
   def update
     review = Review.find(params[:id])
-    review.update(review_params)
-    redirect_to plan_reviews_path(params[:review][:plan_id])
+    if review.update(review_params)
+      redirect_to plan_reviews_path(params[:review][:plan_id])
+    else
+      redirect_to plan_reviews_path(params[:review][:plan_id]), flash: { error: review.errors.full_messages }
+    end
   end
 
   def destroy
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,11 @@
 class Review < ApplicationRecord
 	belongs_to :user
+
+	#validation
+	validates :body, presence: true, length: { maximum: 500 }
+	validates :title, presence: true, length: { maximum: 50 }
+	validates :total_eval, presence: true
+	validates :internet_eval, presence: true
+	validates :support_eval, presence: true
+	validates :cost_eval, presence: true
 end
